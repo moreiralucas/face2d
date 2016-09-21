4c2887925a061e0779b300fd7a22db372854ad5d
Kalyf Abdalla <kalyfabdalla@gmail.com>
2016-09-21 17:22:39 -0300

    Confusion Matrix

--- scripts/eigenfaces.py
+++ scripts/eigenfaces.py
@@ -49,65 +49,93 @@ def loadDataset(path,descriptor,args):
         fli += 1
     return [X,np.array(individuals)]
 
-if __name__=='__main__':
-    print 'Loading Dataset...'
-    X,Y = loadDataset(sys.argv[1],lbp,[24,8])
-    # X_train = X[3:,:]
-    # y_train = Y[3:]
-    # X_test = X[:3]
-    # y_test = Y[:3]
-    labels = np.ones(Y.shape)*-1
-    indexs = filter(lambda indexs : indexs.shape[0]  > 1, [np.where(Y == label)[0] for label in np.unique(Y)])
-    cycle = itertools.cycle(range(0,min([i.shape[0] for i in indexs])))
+def statsByNFace(X,Y):
+    labels = np.ones(Y.shape) * -1
+    indexs = filter(lambda indexs: indexs.shape[0] > 1, [np.where(Y == label)[0] for label in np.unique(Y)])
+    cycle = itertools.cycle(range(0, min([i.shape[0] for i in indexs])))
     for x in indexs:
         cycle_bak = cycle
         labels[x] = [next(cycle_bak) for y in x]
-    #print labels
+    # print labels
     skf = PredefinedSplit(labels)
-    for n_faces in range(1,51):
+    stats = []
+    for n_faces in range(1, 51):
         accs = []
         for train_index, test_index in skf:
-             X_train, X_test = X[train_index], X[test_index]
-             y_train, y_test = Y[train_index], Y[test_index]
-             acerto = 0
-             erro = 0
-             for t_idx in range(len(y_test)):
-                 predicted = match(X_train,y_train,X_test[t_idx])
-                 #print predicted[:3]
-                 predicted_bak = predicted
-                 _,sort = np.unique(predicted_bak.copy(), return_index=True)
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = Y[train_index], Y[test_index]
+            acerto = 0
+            erro = 0
+            for t_idx in range(len(y_test)):
+                predicted = match(X_train, y_train, X_test[t_idx])
+                # print predicted[:3]
+                predicted_bak = predicted
+                _, sort = np.unique(predicted_bak.copy(), return_index=True)
 
-                 predicted = predicted[np.sort(sort)]
-                 #print sort
+                predicted = predicted[np.sort(sort)]
+                # print sort
 
-                 #raw_input('ok')
-                 if np.any(predicted[:n_faces] == y_test[t_idx]):
-                     acerto += 1
-                 else:
-                     erro += 1
-             acc = acerto / float(acerto + erro)
-             #print acerto, erro, acc
-             accs.append(acc)
-        #print accs
+                # raw_input('ok')
+                if np.any(predicted[:n_faces] == y_test[t_idx]):
+                    acerto += 1
+                else:
+                    erro += 1
+            acc = acerto / float(acerto + erro)
+            # print acerto, erro, acc
+            accs.append(acc)
+        # print accs
         accuracy = np.mean(np.array(accs))
-        print (n_faces,accuracy),
+        print (n_faces, accuracy),
+        stats.append((n_faces,accuracy))
+    print ''
+    return stats
 
-    #plt.figure()
-    #for train_index, test_index in skf:
-    #     X_train, X_test = X[train_index], X[test_index]
-    #     y_train, y_test = Y[train_index], Y[test_index]
-    #     print X_train.shape, y_train.shape
-    #
-    #     classifier = SVMLearning(X=X_train, Y=y_train)
-    #     predicted = classifier.guess(X_test)
-    #     cm = confusion_matrix(y_test, predicted)
-    #     np.set_printoptions(precision=2)
-    #     print('Confusion matrix, without normalization')
-    #     print(cm)
-    #     #plot_confusion_matrix(cm)
-    #     #plt.show()
+def matchWithCrossValidation(X,Y):
+    labels = np.ones(Y.shape) * -1
+    indexs = filter(lambda indexs: indexs.shape[0] > 1, [np.where(Y == label)[0] for label in np.unique(Y)])
+    cycle = itertools.cycle(range(0, min([i.shape[0] for i in indexs])))
+    for x in indexs:
+        cycle_bak = cycle
+        labels[x] = [next(cycle_bak) for y in x]
+    # print labels
+    skf = PredefinedSplit(labels)
+    preds = []
+    reals = []
+    for train_index, test_index in skf:
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
+        pred = []
+        real = []
+        for t_idx in range(len(y_test)):
+            predicted = match(X_train, y_train, X_test[t_idx])
+            # print predicted[:3]
+            predicted_bak = predicted
+            _, sort = np.unique(predicted_bak.copy(), return_index=True)
+            predicted = predicted[np.sort(sort)]
+            pred.append(predicted[0])
+            real.append(y_test[t_idx])
+        preds.append(pred)
+        reals.append(real)
+    return np.array(preds),np.array(reals)
 
+if __name__=='__main__':
+    print 'Loading Dataset...'
+    X,Y = loadDataset(sys.argv[1],lbp,[24,8])
+    print 'Stats by Face'
+    accuracy = statsByNFace(X,Y)
+    print 'CM'
+    preds,reals = matchWithCrossValidation(X,Y)
+    plt.figure()
+    cm_avg = np.zeros((50,50))
+    for i in range(preds.shape[0]):
+        cm = confusion_matrix(reals[i],preds[i])
+        cm_avg = np.add(cm,cm_avg)
+    for yi in range(cm_avg.shape[0]):
+        for xi in range(cm_avg.shape[1]):
+            if (xi % 4 == 0):
+                print ''
+            print (yi+1),(xi+1),(cm_avg[yi,xi])
 
-    #print eigenValues.shape, eigenVectors.shape
-
-    #cv2.eigen(X,)
+    np.set_printoptions(precision=2)
+    plot_confusion_matrix(cm_avg)
+    plt.show()
